e8dc4e33923998c791dfa13528420957d28a0ecb
ekaterina-38 <evp38@tpu.ru>
2023-11-30 09:47:33 +0700

    style(Person): изменен комментарий к коду

--- Lab1/Person.cs
+++ Lab1/Person.cs
@@ -26,7 +26,7 @@ namespace Lab1
         private Gender _gender;
 
         /// <summary>
-        /// Конструктор класса Человек.
+        /// Инициализатор класса Человек.
         /// </summary>
         /// <param name="lastName">Фамилия.</param>
         /// <param name="name">Имя.</param>
