eb8da2dd669ca3e380adb6dbee269689e2f178a0
ekaterina-38 <evp38@tpu.ru>
2024-05-14 22:08:40 +0700

    create(LB4): Создана начальная форма

---
+++ WinFormsApp1/BasicForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿using System.Data;
+using System.Windows.Forms;
+
+namespace View
+{
+    partial class BasicForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox = new GroupBox();
+            gridControl = new DataGridView();
+            addTransportButton = new Button();
+            removeTransportButton = new Button();
+            SuspendLayout();
+            // 
+            // gridControl
+            // 
+            gridControl.BackgroundColor = SystemColors.ButtonHighlight;
+            gridControl.Name = "gridControl";
+            gridControl.Dock = DockStyle.Fill;
+            gridControl.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            gridControl.Columns.Add("TypeTransport", "Вид транспорта");
+            gridControl.Columns.Add("Distance", "Расстояние");
+            gridControl.Columns.Add("FuelConsumption", "Расход топлива");
+            // 
+            // addTransportButton
+            // 
+            addTransportButton.BackColor = SystemColors.ButtonHighlight;
+            addTransportButton.ForeColor = SystemColors.ActiveCaptionText;
+            addTransportButton.Location = new Point(444, 379);
+            addTransportButton.Name = "addTransportButton";
+            addTransportButton.Size = new Size(150, 30);
+            addTransportButton.TabIndex = 1;
+            addTransportButton.Text = "Добавить Транспорт";
+            addTransportButton.UseVisualStyleBackColor = false;
+            addTransportButton.Click += addTransportButtonClick;
+            // 
+            // removeTransportButton
+            // 
+            removeTransportButton.BackColor = SystemColors.ButtonHighlight;
+            removeTransportButton.ForeColor = SystemColors.ActiveCaptionText;
+            removeTransportButton.Location = new Point(600, 379);
+            removeTransportButton.Name = "removeTransportButton";
+            removeTransportButton.Size = new Size(150, 30);
+            removeTransportButton.TabIndex = 2;
+            removeTransportButton.Text = "Удалить Транспорт";
+            removeTransportButton.UseVisualStyleBackColor = false;
+            removeTransportButton.Click += removeTransportButtonClick;
+            // 
+            // groupBox
+            // 
+            groupBox.Location = new Point(50, 10);
+            groupBox.Name = "groupBox";
+            groupBox.Size = new Size(700, 352);
+            groupBox.TabIndex = 0;
+            groupBox.TabStop = false;
+            groupBox.Text = "Список Транспорта";
+            groupBox.Controls.Add(gridControl);
+            // 
+            // BasicForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox);
+            Controls.Add(addTransportButton);
+            Controls.Add(removeTransportButton);
+            ForeColor = Color.Bisque;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "BasicForm";
+        }
+
+        private void addTransportButtonClick(object sender, EventArgs e)
+        {
+            NewForm newForm = new NewForm();
+            newForm.Show();
+        }
+
+        private void removeTransportButtonClick(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+    }
+}
---
+++ WinFormsApp1/BasicForm.cs
@@ -0,0 +1,18 @@
+namespace View
+{
+    public partial class BasicForm : Form
+    {
+        private GroupBox groupBox;
+
+        private DataGridView gridControl;
+
+        private Button addTransportButton;
+
+        private Button removeTransportButton;
+
+        public BasicForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
